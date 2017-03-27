ac4bcfb92c529ce2ffbb1debdef5d0ab6a36d8a7
TheFigurehead <alex96813@gmail.com>
2017-03-27 17:47:50 +0300

    v2

--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,22 @@
-<header>
-  header
+<header id="header">
+  <div class="header-wrapper">
+    <div class="header-figure">
+      <div class="logo-wrapper">
+        <img src="images/logo.png" alt="">
+      </div>
+      <img src="images/header-figure.png" alt="">
+    </div>
+    <div class="header-content">
+      <div class="header-content-title">
+        <span>Юридическая компания <br> «Би Кей Би» к Вашим услугам</span>
+      </div>
+      <div class="header-content-content">
+        <span>Штат Компании полностью укомплектован опытными юристами, имеющими достаточный опыт в сфере проведения юридических экспертиз различной сложности</span>
+      </div>
+      <div class="header-content-details">
+        <a href="#"><span class="header-content-details-arrow">⟶</span><span>Подробнее</span></a>
+      </div>
+    </div>
+    <img class="back-img" src="images/header-background.png" alt="">
+  </div>
 </header>
