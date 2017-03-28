807b2459a5ddae11a062208381a40502470d17cc
TheFigurehead <alex96813@gmail.com>
2017-03-28 14:05:06 +0300

    Merge alex and master

--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,60 @@
-<footer>
-  footer
+<footer id="footer">
+  <div class="footer-top-wrapper">
+    <div class="container">
+      <div class="footer-top">
+        <div class="row">
+          <div class="one-third column">
+            <div class="footer-logo-wrapper">
+              <img src="images/logo.png" alt="">
+            </div>
+          </div>
+          <div class="two-thirds column">
+            <div class="footer-menu-wrapper">
+              <ul>
+                <li><a href="#">Правила и условия использования</a></li>
+                <li><a href="#">Ограничения</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Иное</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="one-half column">
+          <div class="copyright-wrapper">
+            <span>© <?php echo date("Y"); ?> «Би Кей Би». Все права защищены.</span>
+          </div>
+        </div>
+        <div class="one-half column">
+          <div class="social-wrapper">
+            <div class="social-list">
+              <div class="social-item">
+                <a class="social-icon vk" href="#"></a>
+              </div>
+              <div class="social-item">
+                <a class="social-icon fb" href="#"></a>
+              </div>
+              <div class="social-item">
+                <a class="social-icon inst" href="#"></a>
+              </div>
+              <div class="social-item">
+                <a class="social-icon twit" href="#"></a>
+              </div>
+              <div class="social-item">
+                <a class="social-icon youtube" href="#"></a>
+              </div>
+              <div class="social-item">
+                <a class="social-icon telegram" href="#"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
--- parts/main_page.php
+++ parts/main_page.php
@@ -1,5 +1,27 @@
 <main id="main">
   <div class="about-company-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="seven columns">
+          <div class="about-company-content">
+            <div class="about-company-title">
+              <h2>О компании</h2>
+              <div class="about-company-title-underline"></div>
+            </div>
+            <div class="about-company-text">
+              <p>Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения законной защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине. Наша Юридическая компания специализируется на предоставлении юридических, бухгалтерских и консалтинговых услуг.</p>
+            </div>
+          </div>
+        </div>
+        <div class="five columns">
+          <div class="company-news">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="our-team-wrapper">
 
   </div>
 </main>
