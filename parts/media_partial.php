54616e949a8b9e4864c51b2fd97b831223899186
Johny-Voo <ilidanaa@gmail.com>
2017-03-28 16:49:56 +0300

    media part create

---
+++ parts/media_partial.php
@@ -0,0 +1,57 @@
+<div class="media-content-wrapper">
+    <div class="container">
+        <div class="row">
+            <p>
+                Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения
+                законной
+                защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине.
+                Наша Юридическая компания специализируется на предоставлении юридических, бухгалтерских и консалтинговых
+                услуг.
+                Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения
+                законной
+                защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине. Наша Юридическая компания
+                специализируется на предоставлении юридических, бухгалтерских и консалтинговых услуг.
+            </p>
+        </div>
+    </div>
+</div>
+<div class="media-social-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="three columns">
+                <script type="text/javascript" src="//vk.com/js/api/openapi.js?143"></script>
+
+                <!-- VK Widget -->
+                <div id="vk_groups"></div>
+                <script type="text/javascript">
+                    VK.Widgets.Group("vk_groups", {mode: 3}, 20003922);
+                </script>
+            </div>
+            <div class="three columns">
+                <div class="fb-follow" data-href="https://www.facebook.com/zuck" data-height="150"
+                     data-layout="box_count" data-size="large" data-show-faces="true"></div>
+            </div>
+            <div class="three columns">
+
+            </div>
+            <div class="three columns">
+                <div class="social-links">
+                    <a href="#">YouTube</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Telegram</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="fb-root"></div>
+<script>(function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.8&appId=1441267802580299";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
