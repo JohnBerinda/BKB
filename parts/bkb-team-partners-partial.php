8888097a31bf2512a80c5d7609ebf9abbeb6325f
Johny-Voo <ilidanaa@gmail.com>
2017-03-29 11:22:19 +0300

    team-partners partial comlite

---
+++ parts/bkb-team-partners-partial.php
@@ -0,0 +1,53 @@
+<div class="advisors-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="four columns">
+                <h5>Советники</h5>
+                <a href="#"><span class="header-content-details-arrow">⟶</span><span>Юристы</span></a>
+                <a href="#"><span class="header-content-details-arrow">⟶</span><span>Советники</span></a>
+            </div>
+            <div class="eight columns">
+                <h2>Работаем с лучшими</h2>
+                <p>Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и
+                    обеспечения законной защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине.
+                    Наша Юридическая компания специализируется на предоставлении юридических, бухгалтерских и
+                    консалтинговых услуг. Юридическая компания «Би Кей Би» была создана в 2010 году для юридического
+                    сопровождения и обеспечения законной защиты широкого круга лиц, имеющих интересы одновременно в
+                    Израиле и Украине. Наша Юридическая компания специализируется на предоставлении юридических,
+                    бухгалтерских и консалтинговых услуг.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="partners-team-wrapper">
+    <div class="container">
+        <h2 class="text-center">Наши партнеры</h2>
+        <div class="block-title-underline"></div>
+        <div class="row">
+            <div class="partners-logo-content">
+                <div class="partners-logo-item">
+                    <a href="#"><img src="images/partners-logo.png" alt="logo"></a>
+                    <p class="text-center">Coca-Cola</p>
+                </div>
+                 <div class="partners-logo-item">
+                    <a href="#"><img src="images/partners-logo.png" alt="logo"></a>
+                     <p class="text-center">Coca-Cola</p>
+                </div>
+                 <div class="partners-logo-item">
+                    <a href="#"><img src="images/partners-logo.png" alt="logo"></a>
+                     <p class="text-center">Coca-Cola</p>
+                </div>
+                 <div class="partners-logo-item">
+                    <a href="#"><img src="images/partners-logo.png" alt="logo"></a>
+                     <p class="text-center">Coca-Cola</p>
+                </div>
+                 <div class="partners-logo-item">
+                    <a href="#"><img src="images/partners-logo.png" alt="logo"></a>
+                     <p class="text-center">Coca-Cola</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
