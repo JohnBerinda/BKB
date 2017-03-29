489feb54b23d1eb5e03fb75ae2062869053b00ad
Johny-Voo <ilidanaa@gmail.com>
2017-03-29 10:34:37 +0300

    bkb-team maket

---
+++ parts/bkb_team_jurist_partial.php
@@ -0,0 +1,39 @@
+<div class="legal-team-wrapper">
+    <div class="container">
+        <div class="legal-team-content-wrapper">
+            <div class="legal-team-content">
+                <div class="row">
+                    <div class="four columns">
+                        <img src="images/bkb-team.jpg" alt="bkb-team">
+
+                            <label>Email:
+                            <a href="mailto:jurist@bkb.com">jurist@bkb.com</a>
+                            </label>
+
+                        <label>Телефон:
+                            <span>+7 (495) 123-23-23</span>
+                        </label>
+                        <div class="info">
+                            <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                            <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                        </div>
+                    </div>
+                    <div class="eight columns">
+                        <h3>Константин Клайман</h3>
+                        <p>Юридическая компания «Би Кей Би» была создана в 2010 году
+                            для юридического сопровождения и обеспечения законной защиты широкого круга лиц, имеющих
+                            интересы одновременно в Израиле
+                            и Украине. Наша Юридическая компания специализируется
+                            на предоставлении юридических, бухгалтерских и консалтинговых услуг. Юридическая компания
+                            «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения законной
+                            защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине. Наша
+                            Юридическая компания специализируется на предоставлении юридических, бухгалтерских и
+                            консалтинговых услуг.</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
--- parts/bkb_team_partial.php
+++ parts/bkb_team_partial.php
@@ -1,39 +1,114 @@
-<div class="legal-team-wrapper">
+<div class="advisors-wrapper">
     <div class="container">
-        <div class="legal-team-content-wrapper">
-            <div class="legal-team-content">
-                <div class="row">
-                    <div class="four columns">
-                        <img src="images/bkb-team.jpg" alt="bkb-team">
+        <div class="row">
+            <div class="four columns">
+                <a href="#"><span class="header-content-details-arrow">⟶</span><span>Партнеры</span></a>
+                <a href="#"><span class="header-content-details-arrow">⟶</span><span>Юристы</span></a>
+                <a href="#"><span class="header-content-details-arrow">⟶</span><span>Советники</span></a>
+            </div>
+            <div class="eight columns">
+                <p>Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и
+                    обеспечения законной защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине.
+                    Наша Юридическая компания специализируется на предоставлении юридических, бухгалтерских и
+                    консалтинговых услуг. Юридическая компания «Би Кей Би» была создана в 2010 году для юридического
+                    сопровождения и обеспечения законной защиты широкого круга лиц, имеющих интересы одновременно в
+                    Израиле и Украине. Наша Юридическая компания специализируется на предоставлении юридических,
+                    бухгалтерских и консалтинговых услуг.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
-                            <label>Email:
-                            <a href="mailto:jurist@bkb.com">jurist@bkb.com</a>
-                            </label>
-
-                        <label>Телефон:
-                            <span>+7 (495) 123-23-23</span>
-                        </label>
-                        <div class="info">
-                            <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
-                            <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
-                        </div>
+<div class="advisors-content-wrapper">
+    <div class="container">
+        <div class="advisors-team-content">
+            <div class="row">
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
                     </div>
-                    <div class="eight columns">
-                        <h3>Константин Клайман</h3>
-                        <p>Юридическая компания «Би Кей Би» была создана в 2010 году
-                            для юридического сопровождения и обеспечения законной защиты широкого круга лиц, имеющих
-                            интересы одновременно в Израиле
-                            и Украине. Наша Юридическая компания специализируется
-                            на предоставлении юридических, бухгалтерских и консалтинговых услуг. Юридическая компания
-                            «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения законной
-                            защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине. Наша
-                            Юридическая компания специализируется на предоставлении юридических, бухгалтерских и
-                            консалтинговых услуг.</p>
+                </div>
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                    </div>
+                </div>
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
                     </div>
                 </div>
             </div>
-
-
+            <div class="row">
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                    </div>
+                </div>
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                    </div>
+                </div>
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                    </div>
+                </div>
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                    </div>
+                </div>
+                <div class="four columns">
+                    <img src="images/bkb-team.jpg" alt="bkb-team">
+                    <h5>Константин Клайман</h5>
+                    <p>Партнёр</p>
+                    <div class="info">
+                        <a href="#"><img src="images/social/fbblue.png" alt="socila"></a>
+                        <a href="#"><img src="images/social/inblue.png" alt="socila"></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
