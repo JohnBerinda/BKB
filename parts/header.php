9a9598a7fa72faabc51a4e51624444c64373b53a
Johny-Voo <ilidanaa@gmail.com>
2017-03-29 16:51:29 +0300

    header slider and fix

--- parts/header.php
+++ parts/header.php
@@ -1,36 +1,65 @@
 <header id="header">
-  <div class="header-wrapper">
-    <nav id="nav">
-      <div class="top-navigation">
-        <ul>
-          <li><a href="#">О нас</a></li>
-          <li><a href="#">Команда</a></li>
-          <li><a href="#">Карьера</a></li>
-          <li><a href="#">Практики</a></li>
-          <li><a href="#">Медиа</a></li>
-          <li><a href="#">Наука и образование</a></li>
-          <li><a href="#">LegolIT</a></li>
-          <li><a href="#">Новости и форум</a></li>
-        </ul>
-      </div>
-    </nav>
-    <div class="header-figure">
-      <div class="logo-wrapper">
-        <img src="images/logo.png" alt="">
-      </div>
-      <img src="images/header-figure.png" alt="">
+    <div class="header-wrapper">
+        <div class="logo-wrapper">
+            <img src="images/logo.png" alt="">
+        </div>
+        <nav id="nav">
+            <div class="top-navigation">
+                <ul>
+                    <li><a href="#">О нас</a></li>
+                    <li><a href="#">Команда</a></li>
+                    <li><a href="#">Карьера</a></li>
+                    <li><a href="#">Практики</a></li>
+                    <li><a href="#">Медиа</a></li>
+                    <li><a href="#">Наука и образование</a></li>
+                    <li><a href="#">LegolIT</a></li>
+                    <li><a href="#">Новости и форум</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="header-figure">
+        </div>
+        <div class="header-slider-list" data-navigation="navbtns1">
+            <div class="crsl-wrap">
+                <div class="crsl-item">
+                    <div class="header-content">
+                        <div class="header-content-title">
+                            <span>Юридическая компания <br> «Би Кей Би» к Вашим услугам</span>
+                        </div>
+                        <div class="header-content-content">
+                            <span>Штат Компании полностью укомплектован опытными юристами, имеющими достаточный опыт в сфере проведения юридических экспертиз различной сложности</span>
+                        </div>
+                        <div class="header-content-details">
+                            <a href="#"><span class="header-content-details-arrow">⟶</span><span>Подробнее</span></a>
+                        </div>
+                    </div>
+                    <img class="back-img" src="images/header-background.png" alt="">
+                </div>
+                <div class="crsl-item">
+                    <div class="header-content">
+                        <div class="header-content-title">
+                            <span>Юридическая компания1 <br> «Би Кей Би» к Вашим услугам</span>
+                        </div>
+                        <div class="header-content-content">
+                            <span>Штат Компании полностью укомплектован опытными юристами, имеющими достаточный опыт в сфере проведения юридических экспертиз различной сложности</span>
+                        </div>
+                        <div class="header-content-details">
+                            <a href="#"><span class="header-content-details-arrow">⟶</span><span>Подробнее</span></a>
+                        </div>
+                    </div>
+                    <img class="back-img" src="images/header-background.png" alt="">
+                </div>
+            </div>
+            <div class="header-slider-navigation" id="navbtns1">
+                <div class="header-slider-navigation-left">
+                    <a href="#" class="previous"><img src="images/slider-arrow1.png" alt="slider-left"></a>
+                </div>
+                <div class="header-slider-navigation-right">
+                    <a href="#" class="next"><img src="images/slider-arrow2.png" alt="slider-right"></a>
+                </div>
+            </div>
+        </div>
+
     </div>
-    <div class="header-content">
-      <div class="header-content-title">
-        <span>Юридическая компания <br> «Би Кей Би» к Вашим услугам</span>
-      </div>
-      <div class="header-content-content">
-        <span>Штат Компании полностью укомплектован опытными юристами, имеющими достаточный опыт в сфере проведения юридических экспертиз различной сложности</span>
-      </div>
-      <div class="header-content-details">
-        <a href="#"><span class="header-content-details-arrow">⟶</span><span>Подробнее</span></a>
-      </div>
-    </div>
-    <img class="back-img" src="images/header-background.png" alt="">
-  </div>
 </header>
+
