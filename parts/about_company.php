a24c40fc805097dda3221a62b2de00250816d3aa
Johny-Voo <ilidanaa@gmail.com>
2017-03-29 13:09:12 +0300

    Add slider maket

--- parts/about_company.php
+++ parts/about_company.php
@@ -1,36 +1,118 @@
-<div class="about-company-benefits">
-  <div class="container">
-    <div class="row">
-      <div class="one-third column">
-        <div class="benefit-card">
-          <div class="benefit-icon-wrapper"><img src="images/about1.png" alt=""></div>
-          <div class="benefit-content"><p>Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения законной.</p></div>
-          <div class="benefit-details"><a href="#">Миссия</a></div>
+<div id="main">
+    <div class="about-company-benefits">
+        <div class="container">
+            <div class="row">
+                <div class="one-third column">
+                    <div class="benefit-card">
+                        <div class="benefit-icon-wrapper"><img src="images/about1.png" alt=""></div>
+                        <div class="benefit-content"><p>Юридическая компания «Би Кей Би» была создана в 2010 году для
+                                юридического сопровождения и обеспечения законной.</p></div>
+                        <div class="benefit-details"><a href="#">Миссия</a></div>
+                    </div>
+                </div>
+                <div class="one-third column">
+                    <div class="benefit-card">
+                        <div class="benefit-icon-wrapper"><img src="images/about2.png" alt=""></div>
+                        <div class="benefit-content"><p>Наша Юридическая компания специализируется на предоставлении
+                                юридических, бухгалтерских и консалтинговых услуг.</p></div>
+                        <div class="benefit-details"><a href="#">История</a></div>
+                    </div>
+                </div>
+                <div class="one-third column">
+                    <div class="benefit-card">
+                        <div class="benefit-icon-wrapper"><img src="images/about3.png" alt=""></div>
+                        <div class="benefit-content"><p>В 2010 году для юридического сопровождения и обеспечения
+                                законной
+                                защиты широкого.</p></div>
+                        <div class="benefit-details"><a href="#">Что дальше?</a></div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="one-third column">
-        <div class="benefit-card">
-          <div class="benefit-icon-wrapper"><img src="images/about2.png" alt=""></div>
-          <div class="benefit-content"><p>Наша Юридическая компания специализируется на предоставлении юридических, бухгалтерских и консалтинговых услуг.</p></div>
-          <div class="benefit-details"><a href="#">История</a></div>
-        </div>
-      </div>
-      <div class="one-third column">
-        <div class="benefit-card">
-          <div class="benefit-icon-wrapper"><img src="images/about3.png" alt=""></div>
-          <div class="benefit-content"><p>В 2010 году для юридического сопровождения и обеспечения законной защиты широкого.</p></div>
-          <div class="benefit-details"><a href="#">Что дальше?</a></div>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
-<div class="about-company-description-wrapper">
-  <div class="container">
-    <div class="about-company-description-content">
-      <p>
-        Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения законной защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине. Наша Юридическая компания специализируется на предоставлении юридических, бухгалтерских и консалтинговых услуг. Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и обеспечения законной защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине Наша Юридическая компания специализируется на предоставлении юридических, бухгалтерских и консалтинговых услуг.
-      </p>
+    <div class="about-company-description-wrapper">
+        <div class="container">
+            <div class="about-company-description-content">
+                <p class="text-center">
+                    Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и
+                    обеспечения
+                    законной защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине. Наша
+                    Юридическая
+                    компания специализируется на предоставлении юридических, бухгалтерских и консалтинговых услуг.
+                    Юридическая компания «Би Кей Би» была создана в 2010 году для юридического сопровождения и
+                    обеспечения
+                    законной защиты широкого круга лиц, имеющих интересы одновременно в Израиле и Украине Наша
+                    Юридическая
+                    компания специализируется на предоставлении юридических, бухгалтерских и консалтинговых услуг.
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
+
+    <div class="about-new-block-wrapper">
+        <h2 class="text-center">Наши новости</h2>
+        <div class="block-title-underline"></div>
+        <div class="slider-news-block-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="four columns">
+                        <img src="images/slider-about-news.jpg" alt="slider">
+                        <div class="about-news-item">
+                            <div class="about-news-item-date">
+                                <div class="about-news-item-date-number">
+                                    <span>31</span>
+                                </div>
+                                <div class="about-news-item-date-month">
+                                    <span>may</span>
+                                </div>
+                            </div>
+                            <div class="about-news-item-link">
+                                <a href="#">Максим Барышников: как технологическая революция меняет все лолка наполеон сталин смотреть без СМС и регистрации</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="four columns">
+                        <img src="images/slider-about-news.jpg" alt="slider">
+                        <div class="about-news-item">
+                            <div class="about-news-item-date">
+                                <div class="about-news-item-date-number">
+                                    <span>31</span>
+                                </div>
+                                <div class="about-news-item-date-month">
+                                    <span>may</span>
+                                </div>
+                            </div>
+                            <div class="about-news-item-link">
+                                <a href="#">Максим Барышников: как технологическая революция меняет все лолка наполеон сталин смотреть без СМС и регистрации</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="four columns">
+                        <img src="images/slider-about-news.jpg" alt="slider">
+                        <div class="about-news-item">
+                            <div class="about-news-item-date">
+                                <div class="about-news-item-date-number">
+                                    <span>31</span>
+                                </div>
+                                <div class="about-news-item-date-month">
+                                    <span>may</span>
+                                </div>
+                            </div>
+                            <div class="about-news-item-link">
+                                <a href="#">Максим Барышников: как технологическая революция меняет все лолка наполеон сталин смотреть без СМС и регистрации</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-slider-navigation">
+                        <div class="about-slider-navigation-left">
+                            <img src="images/slider-control-left.png" alt="slider-left">
+                        </div>
+                        <div class="about-slider-navigation-right">
+                            <img src="images/slider-control-right.png" alt="slider-right">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
